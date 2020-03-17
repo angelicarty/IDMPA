4de50d7c192c767915cda679398f382c8d76efc2
Bunbun <angelicartemis@gmail.com>
2020-03-17 19:07:47 +1100

    nothing significant, some empty scripts,
    pushing it to finish up for the session

---
+++ BearBear/Assets/Scripts/Quest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Quest
+{
+    public int typeOfQuest;
+    public KillQuest killing;
+    //public CollectionQuest collecting;
+}
+
---
+++ BearBear/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    List<KillQuest> quests = new List<KillQuest>();
+
+    public void killed(string monsterName)
+    {
+        //if monster name is in list of quest
+        //add it to kill count
+    }
+
+    public void pickUpQuest(KillQuest quest)
+    {
+
+    }
+
+    public void dropQuest(KillQuest quest)
+    {
+
+    }
+
+}
