03ac315b23d4bb7c18a48dd81f707f7a65a4a352
Bunbun <angelicartemis@gmail.com>
2020-03-27 19:20:40 +1100

    some ui stuff
    dragging, pushing to the front

---
+++ BearBear/Assets/Scripts/DraggableUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableUI : MonoBehaviour, IDragHandler, IBeginDragHandler
+{
+    float offsetX, offsetY;
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        startDrag();
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector3(offsetX + Input.mousePosition.x, offsetY + Input.mousePosition.y, 0);
+
+    }
+
+    void startDrag()
+    {
+        Debug.Log("start");
+        offsetX = transform.position.x - Input.mousePosition.x;
+        offsetY = transform.position.y - Input.mousePosition.y;
+    }
+
+}
---
+++ BearBear/Assets/Scripts/SetUIInFront.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetUIInFront : MonoBehaviour, IPointerDownHandler
+{
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        transform.SetAsLastSibling();
+    }
+}
