0c598173c9aa394f8541eb5d3dfc47792bb5207e
HarryGB <harry.ellem94@gmail.com>
2020-05-17 20:45:12 +1000

    potions

--- BearBear/Assets/Scripts/Item/ItemProperties.cs
+++ BearBear/Assets/Scripts/Item/ItemProperties.cs
@@ -9,6 +9,7 @@ public class ItemProperties : MonoBehaviour
 {
     public string name;
     public int health;
+    public int mana;
     public bool used = false;//temporary flag tripped when item is used to prevent reuse before it can be removed from the inventory, BREAKS IT TODO FIX
     public bool isEdible;
     public int value;//sale value of the item
