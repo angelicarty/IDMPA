3ad7631946418d41b19ac989ed0c1edbe267a96f
Bunbun <angelicartemis@gmail.com>
2020-06-01 22:15:34 +1000

    nothing

---
+++ BearBear/Assets/Scripts/MainMenuTutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuTutorial : MonoBehaviour
+{
+    public GameObject[] tutorials;
+    int pagenum =0;
+    public void nextPage()
+    {
+        tutorials[pagenum].SetActive(false);
+        pagenum++;
+        if(pagenum >= tutorials.Length - 1)
+        {
+            pagenum = 0;
+        }
+        tutorials[pagenum].SetActive(true);
+    }
+
+    public void prevPage()
+    {
+        tutorials[pagenum].SetActive(false);
+        pagenum--;
+        if (pagenum < 0)
+        {
+            pagenum = tutorials.Length - 1;
+        }
+        tutorials[pagenum].SetActive(true);
+    }
+}
